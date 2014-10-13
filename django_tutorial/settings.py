8e993e43514343b4cdbcf9063454c360470a7a1e
Lucas Possatti <lucas_possatti@hotmail.com>
2014-10-13 17:06:21 -0300

    Finalização da segunda parte do tutorial

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -82,3 +82,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
